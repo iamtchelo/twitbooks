27671d71d23ddce424f92ceb05dffb38b41cf8c5
Paulo Costa <paulombcosta@gmail.com>
2019-01-17 14:37:03 +0000

    WIP attempting to get rate limits

--- src/main/kotlin/io/paulocosta/twitbooks/api/TestApi.kt
+++ src/main/kotlin/io/paulocosta/twitbooks/api/TestApi.kt
@@ -2,9 +2,14 @@ package io.paulocosta.twitbooks.api
 
 import io.paulocosta.twitbooks.auth.TwitterAuth
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.ResponseEntity
+import org.springframework.social.twitter.api.RateLimitStatus
+import org.springframework.social.twitter.api.ResourceFamily
+import org.springframework.social.twitter.api.impl.TwitterTemplate
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import java.net.URI
 
 
 @RestController
@@ -14,10 +19,16 @@ class TestApi {
     @Autowired
     lateinit var twitterAuth: TwitterAuth
 
-    @GetMapping
+    @GetMapping("/1")
     fun testAuthenticity(): String {
         val template = twitterAuth.getTemplate()
         return "authenticated: ${template.isAuthorized}"
+    }
+
+    @GetMapping("/2")
+    fun testRateLimit(): MutableMap<ResourceFamily, MutableList<RateLimitStatus>>? {
+        val template = twitterAuth.getTemplate()
+        return template.userOperations().getRateLimitStatus(ResourceFamily.SEARCH)
     }
 
 }
