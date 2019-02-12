1c736236b17bf26099e3763459c01fd6afb82be8
Paulo Costa <paulombcosta@gmail.com>
2019-02-11 23:17:27 -0200

    Saving here because I'm going to be adventurous now

--- src/main/kotlin/io/paulocosta/twitbooks/service/UserService.kt
+++ src/main/kotlin/io/paulocosta/twitbooks/service/UserService.kt
@@ -4,10 +4,22 @@ import io.paulocosta.twitbooks.auth.TwitterProvider
 import io.paulocosta.twitbooks.entity.Friend import io.paulocosta.twitbooks.repository.FriendsRepository
 import mu.KotlinLogging
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.social.twitter.api.CursoredList
 import org.springframework.social.twitter.api.TwitterProfile
 import org.springframework.stereotype.Service
+import java.lang.IllegalStateException
 
 private val logger = KotlinLogging.logger {}
+
+sealed class FriendRequestStatus
+
+object RateLimited : FriendRequestStatus()
+object CursorEnd: FriendRequestStatus()
+data class HasMore(val cursorId: Long): FriendRequestStatus()
+
+enum class SyncStatus {
+    FAIL, SUCCESS
+}
 
 @Service
 class UserService @Autowired constructor(
@@ -18,14 +30,48 @@ class UserService @Autowired constructor(
     /**
      * TODO: check how to iterate the cursor here
      **/
-    fun syncUsers() {
+    fun syncUsers(): SyncStatus {
+        return SyncStatus.FAIL
+    }
+
+    private fun iterateCursor(cursorId: Long, initial: Boolean): FriendRequestStatus {
+        val next = if (initial) {
+            twitterProvider.getTwitter().friendOperations().friends
+        } else {
+            twitterProvider.getTwitter().friendOperations().getFriendsInCursor(cursorId)
+        }
+        val response = handleFriendsResponse(next)
+        return when (response) {
+            is RateLimited -> throw IllegalStateException("Rate limit exceeded")
+            is HasMore -> iterateCursor(response.cursorId, false)
+            is CursorEnd -> CursorEnd
+        }
+    }
+
+    /**
+     * Get the initial list of friends. It provides a cursor for the next iteration if
+     * there is one.
+     **/
+    private fun getInitialFriends(): FriendRequestStatus {
         val rateLimit = rateLimitService.getFriendRateLimits()
-        when (rateLimit.exceeded()) {
-            true -> logger.info { "Rate limit exceeded on friends API" }
+        return when (rateLimit.exceeded()) {
+            true -> {
+                logger.info { "Rate limit exceeded on friends API" }
+                RateLimited
+            }
             false ->  {
                 val friends = twitterProvider.getTwitter().friendOperations().friends
-                saveFriends(friends)
+                handleFriendsResponse(friends)
             }
+        }
+    }
+
+    private fun handleFriendsResponse(friends: CursoredList<TwitterProfile>): FriendRequestStatus {
+        saveFriends(friends)
+        return if (friends.hasNext()) {
+            HasMore(friends.nextCursor)
+        } else {
+            CursorEnd
         }
     }
 
