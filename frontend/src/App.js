eda7ed03762ba2b27adf59c4a25f3c6434305554
Paulo Costa <paulombcosta@gmail.com>
2019-03-10 18:55:21 -0300

    cleaning stuff from App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,8 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 import createStores from "./stores/createStores";
 import { Provider } from "mobx-react";
 import { configure } from 'mobx';
+import BookPage from "./pages/BookPage";
 
 configure({
     enforceActions: true
@@ -15,22 +14,7 @@ class App extends Component {
   render() {
     return (
         <Provider {...stores}>
-          <div className="App">
-            <header className="App-header">
-              <img src={logo} className="App-logo" alt="logo" />
-              <p>
-                Edit <code>src/App.js</code> and save to reload.
-              </p>
-              <a
-                  className="App-link"
-                  href="https://reactjs.org"
-                  target="_blank"
-                  rel="noopener noreferrer"
-              >
-                Learn React
-              </a>
-            </header>
-          </div>
+            <BookPage/>
         </Provider>
     );
   }
---
+++ frontend/src/pages/BookPage.js
@@ -0,0 +1,15 @@
+import React, {Component} from "react";
+
+class BookPage extends Component {
+
+    render() {
+        return(
+            <div>
+                <p>OH HAI</p>
+            </div>
+        )
+    }
+
+}
+
+export default BookPage;
