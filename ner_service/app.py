f4dd546e763dc33b144751e9fd705bdbfe262b57
Paulo Costa <paulombcosta@gmail.com>
2019-04-13 16:42:38 -0300

    ner endpoint

--- ner_service/app.py
+++ ner_service/app.py
@@ -1,6 +1,16 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, jsonify, request
+import spacy
+nlp = spacy.load('en_core_web_md')
 
-@app.route('/')
-def hello():
-    return  'Hello, World!'
+def create_app():
+    app = Flask(__name__)
+
+    @app.route('/process', methods=["POST"])
+    def process():
+        message = request.json['message']
+        doc = nlp(message)
+        ents = [e.text for e in doc.ents if e.label_ == 'WORK_OF_ART']
+        result = {'entities': ents}
+        return jsonify(result)
+
+    return app
