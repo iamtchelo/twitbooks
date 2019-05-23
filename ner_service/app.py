fdb30e36578ac24f2f8b247b37376701b0c93965
Paulo Costa <paulombcosta@gmail.com>
2019-05-23 13:33:22 -0300

    Fixing ner_service not processing properly

--- ner_service/app.py
+++ ner_service/app.py
@@ -2,15 +2,12 @@ from flask import Flask, jsonify, request
 import spacy
 nlp = spacy.load('en_core_web_md')
 
-def create_app():
-    app = Flask(__name__)
+app = Flask(__name__)
 
-    @app.route('/process', methods=["POST"])
-    def process():
-        message = request.json['message']
-        doc = nlp(message)
-        ents = [e.text for e in doc.ents if e.label_ == 'WORK_OF_ART']
-        result = {'entities': ents}
-        return jsonify(result)
-
-    return app
+@app.route('/process', methods=["POST"])
+def process():
+    message = request.json['message']
+    doc = nlp(message)
+    ents = [e.text for e in doc.ents if e.label_ == 'WORK_OF_ART']
+    result = {'entities': ents}
+    return jsonify(result)
