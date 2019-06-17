25c6cafd8a896ce3cfb9f2629539cf3802859393
Paulo Costa <paulombcosta@gmail.com>
2019-06-17 18:48:21 -0300

    WIP

---
+++ frontend/src/stores/MessageStore.test.js
@@ -0,0 +1,12 @@
+const NOOP_CLIENT = {
+    getMessages: () => {
+        return Promise.resolve({
+        })
+    }
+};
+
+test("test set page should normalize the input for the API", () => {
+});
+
+test("get messages should update the store", async () => {
+});
