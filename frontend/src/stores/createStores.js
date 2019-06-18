92c2397bd52a89cfddc23aa3ce890f4b4dc92a84
Paulo Costa <paulombcosta@gmail.com>
2019-06-18 14:47:39 -0300

    WIP devtools support

--- frontend/src/stores/createStores.js
+++ frontend/src/stores/createStores.js
@@ -1,8 +1,10 @@
 import MessageStore from "./MessageStore";
 import client from "../api/client";
 import { BookStore } from "./BookStore";
+import makeInspectable from 'mobx-devtools-mst';
 
 const baseUrl = process.env.REACT_APP_ENDPOINT || "http://localhost:8080/";
+const profile = process.env.REACT_APP_PROFILE || "dev";
 
 const apiClient = client(baseUrl);
 
@@ -18,9 +20,16 @@ const messagesInitialState = {
     totalPages: 0
 };
 
+const debugStore = (store) => {
+    if (profile === "dev") {
+        makeInspectable(store);
+    }
+    return store;
+};
+
 export default function createStores() {
     return {
-        bookStore: BookStore.create(booksInitialState, { client: apiClient }),
-        messageStore: MessageStore.create(messagesInitialState, { client: apiClient })
+        bookStore: debugStore(BookStore.create(booksInitialState, { client: apiClient })),
+        messageStore: debugStore(MessageStore.create(messagesInitialState, { client: apiClient }))
     }
 }
