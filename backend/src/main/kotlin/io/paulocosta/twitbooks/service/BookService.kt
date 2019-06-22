878bec3851cf279ffe5733de13a89f2ae5b7c614
Paulo Costa <paulombcosta@gmail.com>
2019-06-22 19:21:19 -0300

    fixing SQL & matches not being saved because of the profile

--- backend/src/main/kotlin/io/paulocosta/twitbooks/service/BookService.kt
+++ backend/src/main/kotlin/io/paulocosta/twitbooks/service/BookService.kt
@@ -5,16 +5,12 @@ import io.paulocosta.twitbooks.entity.Message
 import io.paulocosta.twitbooks.extensions.toNullable
 import io.paulocosta.twitbooks.repository.BookRepository
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 import org.springframework.stereotype.Service
 
 @Service
 class BookService @Autowired constructor(private val bookRepository: BookRepository) {
-
-    @Value("\${spring.profiles.active}")
-    lateinit var activeProfile: String
 
     fun saveBook(book: Book) {
         bookRepository.save(book)
@@ -37,9 +33,7 @@ class BookService @Autowired constructor(private val bookRepository: BookReposit
     }
 
     fun updateMessages(book: Book, messages: Set<Message>) {
-        if (activeProfile != "prod") {
-            bookRepository.save(book.copy(message = messages))
-        }
+        bookRepository.save(book.copy(message = messages))
     }
 
 }
