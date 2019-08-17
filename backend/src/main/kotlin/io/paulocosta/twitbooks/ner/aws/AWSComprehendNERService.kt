d0b4a68326deda6a91cadd8b2f592ca95de49964
Paulo Costa <paulombcosta@gmail.com>
2019-08-17 16:24:16 -0300

    Improving on the login page a bit

--- backend/src/main/kotlin/io/paulocosta/twitbooks/ner/aws/AWSComprehendNERService.kt
+++ backend/src/main/kotlin/io/paulocosta/twitbooks/ner/aws/AWSComprehendNERService.kt
@@ -11,9 +11,6 @@ import org.springframework.stereotype.Service
 
 val logger = KotlinLogging.logger {}
 
-// NLP requests are measured in units of 100 characters, with a 3 unit (300 character) minimum charge per request.
-// Twitter's limit is 280
-
 @Service
 @Profile("comprehend")
 class AWSComprehendNERService(private val amazonComprehend: AmazonComprehend) : NERService {
@@ -28,8 +25,6 @@ class AWSComprehendNERService(private val amazonComprehend: AmazonComprehend) : 
                 .withText(text)
                 .withLanguageCode("en")
         val result = amazonComprehend.detectEntities(request)
-        result.entities[0].type
-        Thread.sleep(1000)
         logger.info { "Result from AWS Comprehend: $result" }
         return Single.just(result.entities.map { it.text })
     }
--- frontend/src/pages/login/LoginPage.js
+++ frontend/src/pages/login/LoginPage.js
@@ -1,14 +1,20 @@
 import React, { Component } from 'react';
 import { Button } from 'antd';
 import auth0Client from '../../auth/Auth';
+import "./login.css";
 
 class LoginPage extends Component {
 
     render() {
         return (
-            <Button type="primary" className="login-form-button" onClick={() => this.handleClick()}>
-                Log in
-            </Button>
+            <div className="rootContainer">
+                <div className="loginContainer">
+                    <span className="loginText">Twitbooks</span>
+                    <Button type="primary" className="login-form-button loginButton" onClick={() => this.handleClick()}>
+                        Log in
+                    </Button>
+                </div>
+            </div>
         );
     }
 
